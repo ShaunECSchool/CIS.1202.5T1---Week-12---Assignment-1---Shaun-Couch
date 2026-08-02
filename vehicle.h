b31afb97472b5c3020488469e512a8ac1c19fa0b
ShaunECSchool <shaun.couch@sinclair.edu>
2026-08-02 19:57:44 -0400

    cleanup

--- vehicle.h
+++ vehicle.h
@@ -12,16 +12,6 @@ August/1/2026
 using namespace std;
 
 
-//Instructions--------------------------------
-
-/*
-1. Define a base class called Vehicle.  Define the  member variables and method prototypes in Vehicle.h, and the implementation of the methods in Vehicle.cpp.
-    a. Define member variables for manufacturer and year built
-    b. Define a constructor and appropriate getters and setters
-    c. Define a displayInfo method to display the contents of the object
-*/
-
-
 //Class-----------------------------------------
 class Vehicle 
 {
